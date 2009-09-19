2f58cee836cc2ed49830e22f6a20811570cc7d0f
logicmoo <logicmoo@44e199dc-7c7a-11de-a1b3-85d0aeae5c5b>
2009-09-19 19:30:09 +0000

    Incorporated Mark Evenson <evenson@panix.com> GO squasher He expresses my concern also: "But something that easy has me suspicious (was this left out for some other reason)" AAddtional in commit: instead of lots of microwaveable.printStackTraces() it used Debug.trace(microwaveable) Merging of unreadableObjectString method in LispObjekt

--- src/org/armedbear/lisp/AbstractLispObject.java
+++ src/org/armedbear/lisp/AbstractLispObject.java
@@ -1,3 +1,35 @@
+/*
+ * LispObject.java
+ *
+ * Copyright (C) 2002-2007 Peter Graves
+ * $Id: LispObject.java 12149 2009-09-18 06:22:41Z mevenson $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under
+ * terms of your choice, provided that you also meet, for each linked
+ * independent module, the terms and conditions of the license of that
+ * module.  An independent module is a module which is not derived from
+ * or based on this library.  If you modify this library, you may extend
+ * this exception to your version of the library, but you are not
+ * obligated to do so.  If you do not wish to do so, delete this
+ * exception statement from your version.
+ */
 package org.armedbear.lisp;
 
 import java.math.BigInteger;
@@ -621,25 +653,31 @@ abstract public class AbstractLispObject implements LispObject {
 	    return toString();
 	  }
 
-	  public String unreadableString(String s)
-	  {
-	    FastStringBuffer sb = new FastStringBuffer("#<");
-	    sb.append(s);
-	    sb.append(" {");
-	    sb.append(Integer.toHexString(System.identityHashCode(this)).toUpperCase());
-	    sb.append("}>");
-	    return sb.toString();
-	  }
+	  public String unreadableString(String s) {
+		return unreadableString(s, true);
+	}
 
-	  public String unreadableString(Symbol symbol) throws ConditionThrowable
-	  {
-	    FastStringBuffer sb = new FastStringBuffer("#<");
-	    sb.append(symbol.writeToString());
-	    sb.append(" {");
-	    sb.append(Integer.toHexString(System.identityHashCode(this)).toUpperCase());
-	    sb.append("}>");
-	    return sb.toString();
-	  }
+	public String unreadableString(Symbol sym) throws ConditionThrowable {
+		return unreadableString(sym, true);
+	}
+
+	public String unreadableString(String s, boolean identity) {
+		FastStringBuffer sb = new FastStringBuffer("#<");
+		sb.append(s);
+		if (identity) {
+			sb.append(" {");
+			sb.append(Integer.toHexString(System.identityHashCode(this))
+					.toUpperCase());
+			sb.append("}");
+		}
+		sb.append(">");
+		return sb.toString();
+	}
+
+	public String unreadableString(Symbol symbol, boolean identity)
+			throws ConditionThrowable {
+		return unreadableString(symbol.writeToString(), identity);
+	}
 
 	  // Special operator
 	  public LispObject execute(LispObject args, Environment env)
